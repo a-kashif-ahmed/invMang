b7a438375407e36ca5a744652f1dc2f9a37d920e
a-kashif-ahmed <kashifmsd7@gmail.com>
2025-12-23 19:49:20 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 
 import './App.css';
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import Directory from './pages/Directory';
-import Purchases from './pages/Purchases';
+import Homepage from './pages/Homepage';
+import {Route , BrowserRouter, Routes } from 'react-router-dom'
 import Sales from './pages/Sales';
-import { useState } from 'react';
-import Inventory from './pages/Inventory';
-import Threshold from './pages/Threshold';
-import ProfitLoss from './pages/ProfitLoss';
+import Purchases from './pages/Purchases';
 function App() {
-  
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      <Route path='/directory' element={<Directory/>}/>
-      <Route path='/purchases' element={<Purchases/>}/>
-      <Route path='/sales' element={<Sales/>}/>
-      <Route path='/inventory' element={<Inventory/>}/>
-      <Route path='/tbo' element={<Threshold/>}/>
-      <Route path='/pl' element={<ProfitLoss/>}/>
+    <Route path='/' element={<Homepage/>}/>
+    <Route path='/sales' element={<Sales/>}/>
+    <Route path='/purchases' element={<Purchases/>}/>
+    
     </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
